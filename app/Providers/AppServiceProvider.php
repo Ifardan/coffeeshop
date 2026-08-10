ef1b33d47770ddd3a53873cdf20d22a66758972f
Nama Anda <email@gmail.com>
2026-08-11 00:45:34 +0700

    Fix HTTPS URL on Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Sanctum::ignoreMigrations();
+         Sanctum::ignoreMigrations();
+
+         if (app()->environment('production')) {
+            URL::forceScheme('https');
+         }
     }
-}
+}    
